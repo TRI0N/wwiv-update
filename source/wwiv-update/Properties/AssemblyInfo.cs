2f8275eec9f70196b58e4adb7104a9224f71cbca
TRI0N <tri0n@hotmail.com>
2015-12-13 23:32:20 -0800

    Source Fixes

---
+++ source/wwiv-update/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("wwiv-update")]
+[assembly: AssemblyDescription("Automated Update for WWIV5")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Greg's IT Services")]
+[assembly: AssemblyProduct("wwiv-update")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("Greg (TRI0N) Munson")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("06bd83bc-ef6a-4cbc-b09a-1027ca796173")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
